104bbd537adf242d11591ac463ee0e75ae1aadd0
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2025-05-01 13:29:47 -0500

    add test category data

---
+++ src/modules/shared/data/categories.ts
@@ -0,0 +1,46 @@
+import { Category } from "@/modules/common/interfaces/model/category.interface";
+
+export const categoriesData: Category[] = [
+  {
+    id: 1,
+    name: "Casual",
+    slug: "casual",
+    description: "Ropa cómoda para el día a día.",
+    image: "https://images.pexels.com/photos/5331853/pexels-photo-5331853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    name: "Deportiva",
+    slug: "deportiva",
+    description: "Prendas para entrenar o tener un estilo sport.",
+    image: "https://images.pexels.com/photos/13965346/pexels-photo-13965346.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+  {
+    id: 3,
+    name: "Oficina",
+    slug: "oficina",
+    description: "Ropa formal o elegante para el trabajo.",
+    image: "https://images.pexels.com/photos/7698742/pexels-photo-7698742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    name: "Fiesta",
+    slug: "fiesta",
+    description: "Looks para eventos, salidas y celebraciones.",
+    image: "https://images.pexels.com/photos/4663319/pexels-photo-4663319.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    name: "Verano",
+    slug: "verano",
+    description: "Prendas frescas y ligeras para el calor o la playa.",
+    image: "https://images.pexels.com/photos/58592/pexels-photo-58592.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 6,
+    name: "Invierno",
+    slug: "invierno",
+    description: "Abrigos y prendas térmicas para el frío.",
+    image: "https://images.pexels.com/photos/10686400/pexels-photo-10686400.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  },
+];
