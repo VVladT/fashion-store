3cd9a78a0778be8b27658a5fa2a7436675dbfc77
Vladimir Tuñoque <vvladimirtm134@gmail.com>
2025-06-04 13:12:50 -0500

    minor changes

--- src/modules/products/components/page/ProductFilter.tsx
+++ src/modules/products/components/page/ProductFilter.tsx
@@ -57,13 +57,6 @@ const ProductFilter = ({ currentFilters, categories }: Props) => {
     router.push(`/products`);
   };
 
-  useEffect(() => {
-    setTitle(currentFilters.title || "");
-    setPriceMin(currentFilters.price_min || "");
-    setPriceMax(currentFilters.price_max || "");
-    setCategory(currentFilters.category || "");
-  }, [currentFilters]);
-
   return (
     <section className="my-8 px-4">
       <form
